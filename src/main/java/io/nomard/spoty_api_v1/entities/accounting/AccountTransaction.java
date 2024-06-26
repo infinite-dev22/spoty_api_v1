c2bbb4457677503415840c282efe32c65526c660
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-06-26 12:47:36 +0300

    Add AccountTransaction entity to keep track of account actions of Income, Expenditure, Deposit, Debiting and Crediting, Sales and Purchases, returns both sales and purchases.

---
+++ src/main/java/io/nomard/spoty_api_v1/entities/accounting/AccountTransaction.java
@@ -0,0 +1,52 @@
+package io.nomard.spoty_api_v1.entities.accounting;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.nomard.spoty_api_v1.entities.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "account_transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class AccountTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String account;
+
+    @Column(name = "transaction_date")
+    private Date transactionDate;
+    private String credit;
+    private String debit;
+    private String amount;
+    private String note;
+
+    // Deposit, Sale, Payroll, Purchase Returns, Sale Returns, Purchase, Transfer, Expense
+    @Column(name = "transaction_type")
+    private String transactionType;
+
+    @Column(name = "created_at")
+    @JsonIgnore
+    private Date createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by")
+    @JsonIgnore
+    private User createdBy;
+
+    @Column(name = "updated_at")
+    @JsonIgnore
+    private Date updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "updated_by")
+    @JsonIgnore
+    private User updatedBy;
+}
