905995f0b3377f0aa525d3bafa5a712d62d7773f
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-06-27 07:49:08 +0300

    Accounting => Accounts    - add transactions endpoint to accounts.

--- src/main/java/io/nomard/spoty_api_v1/controllers/accounting/AccountController.java
+++ src/main/java/io/nomard/spoty_api_v1/controllers/accounting/AccountController.java
@@ -2,10 +2,12 @@ package io.nomard.spoty_api_v1.controllers.accounting;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.nomard.spoty_api_v1.entities.accounting.Account;
+import io.nomard.spoty_api_v1.entities.accounting.AccountTransaction;
 import io.nomard.spoty_api_v1.errors.NotFoundException;
 import io.nomard.spoty_api_v1.models.FindModel;
 import io.nomard.spoty_api_v1.models.SearchModel;
 import io.nomard.spoty_api_v1.services.implementations.accounting.AccountServiceImpl;
+import io.nomard.spoty_api_v1.services.implementations.accounting.AccountTransactionServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,41 +20,49 @@ import java.util.List;
 @RequestMapping("accounts")
 public class AccountController {
     @Autowired
-    private AccountServiceImpl bankService;
+    private AccountServiceImpl accountService;
+    @Autowired
+    private AccountTransactionServiceImpl accountTransactionService;
 
     @GetMapping("/all")
     public List<Account> getAll(@RequestParam(defaultValue = "0") Integer pageNo,
                                 @RequestParam(defaultValue = "50") Integer pageSize) {
-        return bankService.getAll(pageNo, pageSize);
+        return accountService.getAll(pageNo, pageSize);
     }
 
     @GetMapping("/single")
     public Account getById(@RequestBody FindModel findModel) throws NotFoundException {
-        return bankService.getById(findModel.getId());
+        return accountService.getById(findModel.getId());
     }
 
     @GetMapping("/search")
     public List<Account> getByContains(@RequestBody SearchModel searchModel) {
-        return bankService.getByContains(searchModel.getSearch());
+        return accountService.getByContains(searchModel.getSearch());
     }
 
     @PostMapping("/add")
     public ResponseEntity<ObjectNode> save(@Valid @RequestBody Account account) {
-        return bankService.save(account);
+        return accountService.save(account);
     }
 
     @PutMapping("/update")
     public ResponseEntity<ObjectNode> update(@Valid @RequestBody Account account) throws NotFoundException {
-        return bankService.update(account);
+        return accountService.update(account);
     }
 
     @DeleteMapping("/delete/single")
     public ResponseEntity<ObjectNode> delete(@RequestBody FindModel findModel) {
-        return bankService.delete(findModel.getId());
+        return accountService.delete(findModel.getId());
     }
 
     @DeleteMapping("/delete/multiple")
     public ResponseEntity<ObjectNode> deleteMultiple(@RequestBody ArrayList<Long> ids) throws NotFoundException {
-        return bankService.deleteMultiple(ids);
+        return accountService.deleteMultiple(ids);
+    }
+
+    @GetMapping("/transactions")
+    public List<AccountTransaction> getTransactions(@RequestParam(defaultValue = "0") Integer pageNo,
+                                @RequestParam(defaultValue = "50") Integer pageSize) {
+        return accountTransactionService.getAll(pageNo, pageSize);
     }
 }
