b8de449ed68354038f2bdf9794605ce5ab8a5acc
infinite-dev22 <mwigojm@gmail.com>
2024-09-14 13:05:03 +0300

    Rename adjustmentMaster to adjustment. Add setting of Approved status.

--- src/main/java/io/nomard/spoty_api_v1/services/implementations/adjustments/AdjustmentServiceImpl.java
+++ src/main/java/io/nomard/spoty_api_v1/services/implementations/adjustments/AdjustmentServiceImpl.java
@@ -35,7 +35,7 @@ import java.util.logging.Level;
 @Log
 public class AdjustmentServiceImpl implements AdjustmentService {
     @Autowired
-    private AdjustmentMasterRepository adjustmentMasterRepo;
+    private AdjustmentMasterRepository adjustmentRepo;
     @Autowired
     private AdjustmentTransactionServiceImpl adjustmentTransactionService;
     @Autowired
@@ -52,35 +52,35 @@ public class AdjustmentServiceImpl implements AdjustmentService {
     @Transactional(readOnly = true)
     public Page<AdjustmentMaster> getAll(int pageNo, int pageSize) {
         PageRequest pageRequest = PageRequest.of(pageNo, pageSize, Sort.by(Sort.Order.desc("createdAt")));
-        return adjustmentMasterRepo.findAllByTenantId(authService.authUser().getTenant().getId(), authService.authUser().getId(), pageRequest);
+        return adjustmentRepo.findAllByTenantId(authService.authUser().getTenant().getId(), authService.authUser().getId(), pageRequest);
     }
 
     @Override
     @Cacheable("adjustment_masters")
     @Transactional(readOnly = true)
     public AdjustmentMaster getById(Long id) throws NotFoundException {
-        Optional<AdjustmentMaster> adjustmentMaster = adjustmentMasterRepo.findById(id);
-        if (adjustmentMaster.isEmpty()) {
+        Optional<AdjustmentMaster> adjustment = adjustmentRepo.findById(id);
+        if (adjustment.isEmpty()) {
             throw new NotFoundException();
         }
-        return adjustmentMaster.get();
+        return adjustment.get();
     }
 
     @Override
     @Cacheable("adjustment_masters")
     @Transactional(readOnly = true)
     public List<AdjustmentMaster> getByContains(String search) {
-        return adjustmentMasterRepo.searchAll(authService.authUser().getTenant().getId(), search.toLowerCase());
+        return adjustmentRepo.searchAll(authService.authUser().getTenant().getId(), search.toLowerCase());
     }
 
     @Override
     @Transactional
-    public ResponseEntity<ObjectNode> save(AdjustmentMaster adjustmentMaster) {
-        CoreCalculations.AdjustmentCalculationService.calculate(adjustmentMaster);
-        adjustmentMaster.setRef(CoreUtils.referenceNumberGenerator("ADJ"));
-        adjustmentMaster.setTenant(authService.authUser().getTenant());
-        if (Objects.isNull(adjustmentMaster.getBranch())) {
-            adjustmentMaster.setBranch(authService.authUser().getBranch());
+    public ResponseEntity<ObjectNode> save(AdjustmentMaster adjustment) {
+        CoreCalculations.AdjustmentCalculationService.calculate(adjustment);
+        adjustment.setRef(CoreUtils.referenceNumberGenerator("ADJ"));
+        adjustment.setTenant(authService.authUser().getTenant());
+        if (Objects.isNull(adjustment.getBranch())) {
+            adjustment.setBranch(authService.authUser().getBranch());
         }
         if (settingsService.getSettings().getApproveAdjustments()) {
             Approver approver = null;
@@ -90,25 +90,27 @@ public class AdjustmentServiceImpl implements AdjustmentService {
                 log.log(Level.ALL, e.getMessage(), e);
             }
             if (Objects.nonNull(approver)) {
-                adjustmentMaster.getApprovers().add(approver);
-                adjustmentMaster.setLatestApprovedLevel(approver.getLevel());
+                adjustment.getApprovers().add(approver);
+                adjustment.setLatestApprovedLevel(approver.getLevel());
                 if (approver.getLevel() >= settingsService.getSettings().getApprovalLevels()) {
-                    adjustmentMaster.setApproved(true);
+                    adjustment.setApproved(true);
+                    adjustment.setApprovalStatus("Approved");
                 }
             } else {
-                adjustmentMaster.setApproved(false);
+                adjustment.setApproved(false);
             }
-            adjustmentMaster.setApprovalStatus("Pending");
+            adjustment.setApprovalStatus("Pending");
         } else {
-            adjustmentMaster.setApproved(true);
+            adjustment.setApproved(true);
+            adjustment.setApprovalStatus("Approved");
         }
-        adjustmentMaster.setCreatedBy(authService.authUser());
-        adjustmentMaster.setCreatedAt(LocalDateTime.now());
+        adjustment.setCreatedBy(authService.authUser());
+        adjustment.setCreatedAt(LocalDateTime.now());
 
         try {
-            adjustmentMasterRepo.save(adjustmentMaster);
-            for (int i = 0; i < adjustmentMaster.getAdjustmentDetails().size(); i++) {
-                adjustmentTransactionService.save(adjustmentMaster.getAdjustmentDetails().get(i));
+            adjustmentRepo.save(adjustment);
+            for (int i = 0; i < adjustment.getAdjustmentDetails().size(); i++) {
+                adjustmentTransactionService.save(adjustment.getAdjustmentDetails().get(i));
             }
             return spotyResponseImpl.created();
         } catch (Exception e) {
@@ -121,34 +123,35 @@ public class AdjustmentServiceImpl implements AdjustmentService {
     @CacheEvict(value = "adjustment_masters", key = "#data.id")
     @Transactional
     public ResponseEntity<ObjectNode> update(AdjustmentMaster data) throws NotFoundException {
-        var opt = adjustmentMasterRepo.findById(data.getId());
+        var opt = adjustmentRepo.findById(data.getId());
         if (opt.isEmpty()) {
             throw new NotFoundException();
         }
-        var adjustmentMaster = opt.get();
+        var adjustment = opt.get();
         if (Objects.nonNull(data.getBranch())) {
-            adjustmentMaster.setBranch(data.getBranch());
+            adjustment.setBranch(data.getBranch());
         }
         if (Objects.nonNull(data.getAdjustmentDetails()) && !data.getAdjustmentDetails().isEmpty()) {
-            adjustmentMaster.setAdjustmentDetails(data.getAdjustmentDetails());
-            CoreCalculations.AdjustmentCalculationService.calculate(adjustmentMaster);
+            adjustment.setAdjustmentDetails(data.getAdjustmentDetails());
+            CoreCalculations.AdjustmentCalculationService.calculate(adjustment);
         }
         if (Objects.nonNull(data.getRef()) && !"".equalsIgnoreCase(data.getRef())) {
-            adjustmentMaster.setRef(data.getRef());
+            adjustment.setRef(data.getRef());
         }
         if (Objects.nonNull(data.getNotes()) && !"".equalsIgnoreCase(data.getNotes())) {
-            adjustmentMaster.setNotes(data.getNotes());
+            adjustment.setNotes(data.getNotes());
         }
         if (Objects.nonNull(data.getApprovers()) && !data.getApprovers().isEmpty()) {
-            adjustmentMaster.getApprovers().add(data.getApprovers().getFirst());
-            if (adjustmentMaster.getLatestApprovedLevel() >= settingsService.getSettings().getApprovalLevels()) {
-                adjustmentMaster.setApproved(true);
+            adjustment.getApprovers().add(data.getApprovers().getFirst());
+            if (adjustment.getLatestApprovedLevel() >= settingsService.getSettings().getApprovalLevels()) {
+                adjustment.setApproved(true);
+                adjustment.setApprovalStatus("Approved");
             }
         }
-        adjustmentMaster.setUpdatedBy(authService.authUser());
-        adjustmentMaster.setUpdatedAt(LocalDateTime.now());
+        adjustment.setUpdatedBy(authService.authUser());
+        adjustment.setUpdatedAt(LocalDateTime.now());
         try {
-            adjustmentMasterRepo.save(adjustmentMaster);
+            adjustmentRepo.save(adjustment);
             return spotyResponseImpl.ok();
         } catch (Exception e) {
             log.log(Level.ALL, e.getMessage(), e);
@@ -160,36 +163,37 @@ public class AdjustmentServiceImpl implements AdjustmentService {
     @CacheEvict(value = "adjustment_masters", key = "#approvalModel.id")
     @Transactional
     public ResponseEntity<ObjectNode> approve(ApprovalModel approvalModel) throws NotFoundException {
-        var opt = adjustmentMasterRepo.findById(approvalModel.getId());
+        var opt = adjustmentRepo.findById(approvalModel.getId());
         if (opt.isEmpty()) {
             throw new NotFoundException();
         }
-        var adjustmentMaster = opt.get();
+        var adjustment = opt.get();
 
         if (Objects.equals(approvalModel.getStatus().toLowerCase(), "returned")) {
-            adjustmentMaster.setApproved(false);
-            adjustmentMaster.setApprovalStatus("Returned");
+            adjustment.setApproved(false);
+            adjustment.setApprovalStatus("Returned");
         }
 
         if (Objects.equals(approvalModel.getStatus().toLowerCase(), "approved")) {
             var approver = approverService.getByUserId(authService.authUser().getId());
-            adjustmentMaster.getApprovers().add(approver);
-            adjustmentMaster.setLatestApprovedLevel(approver.getLevel());
-            if (adjustmentMaster.getLatestApprovedLevel() >= settingsService.getSettings().getApprovalLevels()) {
-                adjustmentMaster.setApproved(true);
+            adjustment.getApprovers().add(approver);
+            adjustment.setLatestApprovedLevel(approver.getLevel());
+            if (adjustment.getLatestApprovedLevel() >= settingsService.getSettings().getApprovalLevels()) {
+                adjustment.setApproved(true);
+                adjustment.setApprovalStatus("Approved");
             }
         }
 
         if (Objects.equals(approvalModel.getStatus().toLowerCase(), "rejected")) {
-            adjustmentMaster.setApproved(false);
-            adjustmentMaster.setApprovalStatus("Rejected");
-            adjustmentMaster.setLatestApprovedLevel(0);
+            adjustment.setApproved(false);
+            adjustment.setApprovalStatus("Rejected");
+            adjustment.setLatestApprovedLevel(0);
         }
 
-        adjustmentMaster.setUpdatedBy(authService.authUser());
-        adjustmentMaster.setUpdatedAt(LocalDateTime.now());
+        adjustment.setUpdatedBy(authService.authUser());
+        adjustment.setUpdatedAt(LocalDateTime.now());
         try {
-            adjustmentMasterRepo.save(adjustmentMaster);
+            adjustmentRepo.save(adjustment);
             return spotyResponseImpl.ok();
         } catch (Exception e) {
             log.log(Level.ALL, e.getMessage(), e);
@@ -201,7 +205,7 @@ public class AdjustmentServiceImpl implements AdjustmentService {
     @Transactional
     public ResponseEntity<ObjectNode> delete(Long id) {
         try {
-            adjustmentMasterRepo.deleteById(id);
+            adjustmentRepo.deleteById(id);
             return spotyResponseImpl.ok();
         } catch (Exception e) {
             log.log(Level.ALL, e.getMessage(), e);
@@ -212,7 +216,7 @@ public class AdjustmentServiceImpl implements AdjustmentService {
     @Override
     public ResponseEntity<ObjectNode> deleteMultiple(List<Long> idList) {
         try {
-            adjustmentMasterRepo.deleteAllById(idList);
+            adjustmentRepo.deleteAllById(idList);
             return spotyResponseImpl.ok();
         } catch (Exception e) {
             log.log(Level.ALL, e.getMessage(), e);
