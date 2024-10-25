9e2321422c146536c822bef630dd9de28894928c
infinite-dev22 <mwigojm@gmail.com>
2024-10-25 10:22:22 +0300

    Add ProductCategoryMapper and ProductCategoryDTO

---
+++ src/main/java/io/nomard/spoty_api_v1/entities/json_mapper/mappers/ProductCategoryMapper.java
@@ -0,0 +1,12 @@
+package io.nomard.spoty_api_v1.entities.json_mapper.mappers;
+
+import io.nomard.spoty_api_v1.entities.ProductCategory;
+import io.nomard.spoty_api_v1.entities.deductions.Discount;
+import io.nomard.spoty_api_v1.entities.json_mapper.dto.DiscountDTO;
+import io.nomard.spoty_api_v1.entities.json_mapper.dto.ProductCategoryDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductCategoryMapper {
+    ProductCategoryDTO toDTO(ProductCategory productCategory);
+}
