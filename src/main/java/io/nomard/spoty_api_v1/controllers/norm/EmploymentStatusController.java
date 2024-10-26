749225e0901435007912cb4b314f9b1566387e20
infinite-dev22 <mwigojm@gmail.com>
2024-10-26 21:52:51 +0300

    Add employment status mapping for explicit control over the employment status json creation.

--- src/main/java/io/nomard/spoty_api_v1/controllers/norm/EmploymentStatusController.java
+++ src/main/java/io/nomard/spoty_api_v1/controllers/norm/EmploymentStatusController.java
@@ -1,12 +1,13 @@
 package io.nomard.spoty_api_v1.controllers.norm;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.nomard.spoty_api_v1.entities.hrm.hrm.EmploymentStatus;
+import io.nomard.spoty_api_v1.entities.json_mapper.dto.EmploymentStatusDTO;
 import io.nomard.spoty_api_v1.errors.NotFoundException;
 import io.nomard.spoty_api_v1.models.FindModel;
 import io.nomard.spoty_api_v1.models.SearchModel;
 import io.nomard.spoty_api_v1.services.implementations.hrm.hrm.EmploymentStatusServiceImpl;
-import com.fasterxml.jackson.annotation.JsonView;
 import io.nomard.spoty_api_v1.utils.Views;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,18 +26,18 @@ public class EmploymentStatusController {
 
     @GetMapping("/all")
     @JsonView(Views.Tiny.class)
-    public Page<EmploymentStatus> getAll(@RequestParam(defaultValue = "0") Integer pageNo,
-                                         @RequestParam(defaultValue = "50") Integer pageSize) {
+    public Page<EmploymentStatusDTO.EmploymentStatusAsWholeDTO> getAll(@RequestParam(defaultValue = "0") Integer pageNo,
+                                                                       @RequestParam(defaultValue = "50") Integer pageSize) {
         return employmentStatusService.getAll(pageNo, pageSize);
     }
 
     @GetMapping("/single")
-    public EmploymentStatus getById(@RequestBody FindModel findModel) throws NotFoundException {
+    public EmploymentStatusDTO.EmploymentStatusAsWholeDTO getById(@RequestBody FindModel findModel) throws NotFoundException {
         return employmentStatusService.getById(findModel.getId());
     }
 
     @GetMapping("/search")
-    public List<EmploymentStatus> getByContains(@RequestBody SearchModel searchModel) {
+    public List<EmploymentStatusDTO.EmploymentStatusAsWholeDTO> getByContains(@RequestBody SearchModel searchModel) {
         return employmentStatusService.getByContains(searchModel.getSearch());
     }
 
--- src/main/java/io/nomard/spoty_api_v1/services/interfaces/hrm/hrm/EmploymentStatusService.java
+++ src/main/java/io/nomard/spoty_api_v1/services/interfaces/hrm/hrm/EmploymentStatusService.java
@@ -2,6 +2,7 @@ package io.nomard.spoty_api_v1.services.interfaces.hrm.hrm;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.nomard.spoty_api_v1.entities.hrm.hrm.EmploymentStatus;
+import io.nomard.spoty_api_v1.entities.json_mapper.dto.EmploymentStatusDTO;
 import io.nomard.spoty_api_v1.errors.NotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +10,11 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 public interface EmploymentStatusService {
-    Page<EmploymentStatus> getAll(int pageNo, int pageSize);
+    Page<EmploymentStatusDTO.EmploymentStatusAsWholeDTO> getAll(int pageNo, int pageSize);
 
-    EmploymentStatus getById(Long id) throws NotFoundException;
+    EmploymentStatusDTO.EmploymentStatusAsWholeDTO getById(Long id) throws NotFoundException;
 
-    List<EmploymentStatus> getByContains(String search);
+    List<EmploymentStatusDTO.EmploymentStatusAsWholeDTO> getByContains(String search);
 
     ResponseEntity<ObjectNode> save(EmploymentStatus employmentStatus);
 
